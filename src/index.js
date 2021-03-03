d5cb45622db521eaa95e214e8c396d3114a3d140
BogdanBalanar <badikradik123@gmail.com>
2021-03-03 09:44:41 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 
 exports.min = function min (array) {
-  return 0;
+ if (array === undefined) return 0
+  return(array.length===0)? 0:array.slice().sort((a,b)=>a-b)[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined) return 0
+  return(array.length===0)? 0:array.slice().sort((a,b)=>b-a)[0];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined) return 0
+  let sum=0;
+  for(let i=0;i<array.length;i++){
+    sum+=array[i];
+  }
+  return(array.length===0 || array === undefined)?0:sum/array.length;
 }
